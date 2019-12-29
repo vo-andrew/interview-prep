ce4d65349ec05d8b93644b95c10e46e204a2a89d
Andrew Vo <voandrew@berkeley.edu>
2019-12-29 12:06:51 -0800

    Completed daily coding problems 13 and 14.

---
+++ p14.py
@@ -0,0 +1,26 @@
+"""
+The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+
+Hint: The basic equation of a circle is x2 + y2 = r2.
+"""
+
+from random import random
+radius = 2
+
+
+def estimate_pi(num_random_tests):
+    pi_counter = 0
+    rsquared = radius ** 2
+    for _ in range(num_random_tests):
+        x_rand = random() * radius
+        y_rand = random() * radius
+        if (x_rand ** 2) + (y_rand ** 2) < rsquared:
+            pi_counter += 1
+
+    return 4 * pi_counter / num_random_tests
+
+
+assert round(estimate_pi(100000000), 3) == 3.141
+
+# Time Complexity: O(N) because we run this estimator based on the input value of num_random_tests and also based on how much accuracy we want.
+# Space Complexity: O(1) because this is an iterative solution that not does allocate any extra memory for data structures.
