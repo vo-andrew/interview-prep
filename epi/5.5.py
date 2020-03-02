320afa13a37659dc9905d06052b220dff4ed15c4
Andrew Vo <voandrew@berkeley.edu>
2020-03-02 01:08:45 -0800

    Completed problem 5.5.

--- epi/5.5.py
+++ epi/5.5.py
@@ -1,52 +1,23 @@
 """
 Delete Duplicates From A Sorted Array
-Input: 
-    arr: [2,3,5,5,7,11,11,11,13]
-Output: 
-    return: 6
-    arr: [2,3,5,7,11,13,0,0,0]
+Input:
+    A sorted array with duplicate values.
+Output:
+    An array with the duplicate values removed.
 """
-"""
-len = 6
-duplicate = 3
-[2,3,5,7,11,13,13,13,13]
-"""
-def my_solution(arr):
-    if arr is None or len(arr) < 2:
-       return 0 
-    window = len(arr)
-    duplicates = 0
-    i = 1
-    # for i in range(1, window): This causes an infinite loop. Window is not being updated somehow.
-    while i < window:
-        while arr[i] == arr[i - 1]:
-            window -= 1
-            duplicates += 1
-            for j in range(i, window):
-                arr[j] = arr[j + 1]
-        i += 1
-    return len(arr) - duplicates
-            
-arr = [2,3,5,5,7,11,11,11,13]
-print(my_solution(arr))
-print(arr)
 
-# Time Complexity: O(N^2) because the number of shifts we have to do is (n - 1) + (n - 2) + ... + 2 + 1.
-# Space Complexity: O(1) because we do not allocate any extra memory.
-
-def delete_duplicates(A):
-    if not A:
-        return 0
-    write_index = 1
+def solution(A): 
+    """
+    We want to overwrite duplicate values with new values that we see in one pass.
+    """
+    available = 1     
     for i in range(1, len(A)):
-        if A[write_index - 1] != A[i]:
-            A[write_index] = A[i]
-            write_index += 1
-    return write_index
+        if A[available - 1] != A[i]:
+            A[available] = A[i]
+            available += 1
+    return A
 
-arr = [2,3,5,5,7,11,11,11,13]
-print(delete_duplicates(arr))
-print(arr)
+print(solution([2, 3, 5, 5, 7, 11, 11, 11, 13]))
 
-# Time Complexity: O(N) because we have reduced the number of shifts needed when writing over duplicate values.
-# Space Complexity: O(1) because we do not allocate any extra memory.
+# Time Compelxity: O(N) because we traverse through the array in one pass.
+# Space Complexity: O(1) because we do not allocate any extra memory to hold data.
